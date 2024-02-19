431806843fa0024df665e04143cec357cbc8fd02
Sourov-HossainDev <sourovhossain2326@gmail.com>
2024-02-19 11:06:08 +0600

    friendPage change

--- src/Component/Friend/FriendPage.jsx
+++ src/Component/Friend/FriendPage.jsx
@@ -4,23 +4,151 @@ import ManPicture from '../../assets/Image/manpicture.jpg'
 const FriendPage = () => {
   return (
     <>
-        <div className='bg-primaryColor w-full h-screen  '>
-          <div className='max-w-container mx-auto bg-sceondColor text-offWhite px-[30px] py-[50px]  '>
-            <div className='flex justify-center font-lobster text-4xl mb-[10px]  '>
-              
-              <p>User Name</p>
-              <p> - All Friend</p>
-              
-            </div>
-            <div className='border-y-4 py-[3px] mb-[50px] '></div>
-            <div>
-              <div className='w-[240px] h-[250px] bg-primaryColor rounded-[8.6px] py-[20px] px-[10px] '>
-                <img src={ManPicture} alt="" />
-                <p className='font-Raleway font-semibold text-base mt-[10px]   '>Md Hasibur Rhaman</p>
+      <div className='bg-primaryColor w-full h-full  '>
+        <div className='max-w-container mx-auto bg-sceondColor text-offWhite px-[30px] py-[50px]  '>
+          <div className='flex justify-center font-lobster text-4xl mb-[10px]  '>
+
+            <p>User Name</p>
+            <p> - All Friend</p>
+
+          </div>
+          <div className='border-y-4 py-[3px] mb-[50px] '></div>
+
+          <div className='flex justify-between '>
+            <div className='w-1/4 flex justify-center'>
+              <div>
+                <div className='w-[240px] h-[250px] bg-primaryColor rounded-[8.6px]  '>
+                  <div className='py-[20px] px-[10px] w-full '>
+                    <img src={ManPicture} alt="" className='w-full ' />
+                    <p className='font-Raleway font-semibold text-xl mt-[20px] text-center   '>Md Hasibur Rhaman</p>
+                  </div>
+
+                </div>
+                <div className='w-[240px] h-[250px] bg-primaryColor rounded-[8.6px] mt-[30px]  '>
+                  <div className='py-[20px] px-[10px] w-full '>
+                    <img src={ManPicture} alt="" className='w-full ' />
+                    <p className='font-Raleway font-semibold text-xl mt-[20px] text-center   '>Md Hasibur Rhaman</p>
+                  </div>
+
+                </div>
+                <div className='w-[240px] h-[250px] bg-primaryColor rounded-[8.6px] mt-[30px]  '>
+                  <div className='py-[20px] px-[10px] w-full '>
+                    <img src={ManPicture} alt="" className='w-full ' />
+                    <p className='font-Raleway font-semibold text-xl mt-[20px] text-center   '>Md Hasibur Rhaman</p>
+                  </div>
+
+                </div>
+                <div className='w-[240px] h-[250px] bg-primaryColor rounded-[8.6px] mt-[30px]  '>
+                  <div className='py-[20px] px-[10px] w-full '>
+                    <img src={ManPicture} alt="" className='w-full ' />
+                    <p className='font-Raleway font-semibold text-xl mt-[20px] text-center   '>Md Hasibur Rhaman</p>
+                  </div>
+
+                </div>
               </div>
             </div>
+            <div className='w-1/4 flex justify-center'>
+              <div>
+                <div className='w-[240px] h-[250px] bg-primaryColor rounded-[8.6px]  '>
+                  <div className='py-[20px] px-[10px] w-full '>
+                    <img src={ManPicture} alt="" className='w-full ' />
+                    <p className='font-Raleway font-semibold text-xl mt-[20px] text-center   '>Md Hasibur Rhaman</p>
+                  </div>
+
+                </div>
+                <div className='w-[240px] h-[250px] bg-primaryColor rounded-[8.6px] mt-[30px]  '>
+                  <div className='py-[20px] px-[10px] w-full '>
+                    <img src={ManPicture} alt="" className='w-full ' />
+                    <p className='font-Raleway font-semibold text-xl mt-[20px] text-center   '>Md Hasibur Rhaman</p>
+                  </div>
+
+                </div>
+                <div className='w-[240px] h-[250px] bg-primaryColor rounded-[8.6px] mt-[30px]  '>
+                  <div className='py-[20px] px-[10px] w-full '>
+                    <img src={ManPicture} alt="" className='w-full ' />
+                    <p className='font-Raleway font-semibold text-xl mt-[20px] text-center   '>Md Hasibur Rhaman</p>
+                  </div>
+
+                </div>
+                <div className='w-[240px] h-[250px] bg-primaryColor rounded-[8.6px] mt-[30px]  '>
+                  <div className='py-[20px] px-[10px] w-full '>
+                    <img src={ManPicture} alt="" className='w-full ' />
+                    <p className='font-Raleway font-semibold text-xl mt-[20px] text-center   '>Md Hasibur Rhaman</p>
+                  </div>
+
+                </div>
+              </div>
+            </div>
+            <div className='w-1/4 flex justify-center'>
+              <div>
+                <div className='w-[240px] h-[250px] bg-primaryColor rounded-[8.6px]  '>
+                  <div className='py-[20px] px-[10px] w-full '>
+                    <img src={ManPicture} alt="" className='w-full ' />
+                    <p className='font-Raleway font-semibold text-xl mt-[20px] text-center   '>Md Hasibur Rhaman</p>
+                  </div>
+
+                </div>
+                <div className='w-[240px] h-[250px] bg-primaryColor rounded-[8.6px] mt-[30px]  '>
+                  <div className='py-[20px] px-[10px] w-full '>
+                    <img src={ManPicture} alt="" className='w-full ' />
+                    <p className='font-Raleway font-semibold text-xl mt-[20px] text-center   '>Md Hasibur Rhaman</p>
+                  </div>
+
+                </div>
+                <div className='w-[240px] h-[250px] bg-primaryColor rounded-[8.6px] mt-[30px]  '>
+                  <div className='py-[20px] px-[10px] w-full '>
+                    <img src={ManPicture} alt="" className='w-full ' />
+                    <p className='font-Raleway font-semibold text-xl mt-[20px] text-center   '>Md Hasibur Rhaman</p>
+                  </div>
+
+                </div>
+                <div className='w-[240px] h-[250px] bg-primaryColor rounded-[8.6px] mt-[30px]  '>
+                  <div className='py-[20px] px-[10px] w-full '>
+                    <img src={ManPicture} alt="" className='w-full ' />
+                    <p className='font-Raleway font-semibold text-xl mt-[20px] text-center   '>Md Hasibur Rhaman</p>
+                  </div>
+
+                </div>
+              </div>
+            </div>
+            <div className='w-1/4 flex justify-center'>
+              <div>
+                <div className='w-[240px] h-[250px] bg-primaryColor rounded-[8.6px]  '>
+                  <div className='py-[20px] px-[10px] w-full '>
+                    <img src={ManPicture} alt="" className='w-full ' />
+                    <p className='font-Raleway font-semibold text-xl mt-[20px] text-center   '>Md Hasibur Rhaman</p>
+                  </div>
+
+                </div>
+                <div className='w-[240px] h-[250px] bg-primaryColor rounded-[8.6px] mt-[30px]  '>
+                  <div className='py-[20px] px-[10px] w-full '>
+                    <img src={ManPicture} alt="" className='w-full ' />
+                    <p className='font-Raleway font-semibold text-xl mt-[20px] text-center   '>Md Hasibur Rhaman</p>
+                  </div>
+
+                </div>
+                <div className='w-[240px] h-[250px] bg-primaryColor rounded-[8.6px] mt-[30px]  '>
+                  <div className='py-[20px] px-[10px] w-full '>
+                    <img src={ManPicture} alt="" className='w-full ' />
+                    <p className='font-Raleway font-semibold text-xl mt-[20px] text-center   '>Md Hasibur Rhaman</p>
+                  </div>
+
+                </div>
+                <div className='w-[240px] h-[250px] bg-primaryColor rounded-[8.6px] mt-[30px]  '>
+                  <div className='py-[20px] px-[10px] w-full '>
+                    <img src={ManPicture} alt="" className='w-full ' />
+                    <p className='font-Raleway font-semibold text-xl mt-[20px] text-center   '>Md Hasibur Rhaman</p>
+                  </div>
+
+                </div>
+              </div>
+            </div>
+           
+
+
           </div>
         </div>
+      </div>
     </>
   )
 }
